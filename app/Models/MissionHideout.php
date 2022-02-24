5b86acf6e08039ebe17c55866d2738f3d9eba3e7
Ludovic JORGE DO MARCO <48907643+Slipndi@users.noreply.github.com>
2022-02-24 12:48:44 +0100

    Mise à jour du lien MISSION et MISSION HIDEOUT

--- app/Models/MissionHideout.php
+++ app/Models/MissionHideout.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MissionHideout extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'hideout_id',
+        'mission_id'
+    ];
+
+    /**
+     * Crée le liens entre HIDEOUTS et MISSIONS
+     *
+     * @return BelongsTo
+     */
+    public function hideouts() : BelongsTo {
+        return $this->belongsTo(Hideout::class);
+    }
+    
+    /**
+     * Crée le liens entre HIDEOUTS et MISSIONS
+     *
+     * @return BelongsTo
+     */
+    public function missions() : BelongsTo {
+        return $this->belongsTo(Mission::class);
+    }
+
 }
