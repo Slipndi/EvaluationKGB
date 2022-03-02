9aa93cfd8f184694132c87e0a0359e8fdae8ba79
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-03-02 21:38:24 +0100

    update de la migration et du controller pour la gestion des planques

--- app/Http/Controllers/InitiateMissionController.php
+++ app/Http/Controllers/InitiateMissionController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ { Country, Mission, MissionPerson};
+use App\Models\ {Mission, MissionHideout, MissionPerson};
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\ { JsonResponse, Request };
 use Illuminate\Support\Collection;
@@ -13,6 +13,11 @@ class InitiateMissionController extends Controller
     protected const ON_PROGRESS_STATUT = 2;
     protected int $missionId;
     
+    /**
+     * Display information into view
+     *
+     * @return View
+     */
     public function index() : View {
         $missions = Mission::select('missions.*', 'specialities.speciality_name')
             ->where('statut_id', static::ON_PREPARE_STATUT)
@@ -32,9 +37,15 @@ class InitiateMissionController extends Controller
         $statut = static::ON_PROGRESS_STATUT;
         $formData = collect($request->all());
         $this->missionId = json_decode($formData['mission'])->id;
+
         $targets = collect($formData['targets']);
         $agents = collect($formData['agents']);
         $contacts =collect($formData['contacts']);
+
+        if ($formData->has('hideouts')){
+            $hideouts = collect($formData['hideouts']);
+            $this->insertHideoutMission($hideouts);
+        } 
 
         // Je regroupe tous les intervenants de la mission puis insertion en bdd
         $people = $targets->merge($agents)->merge($contacts);
@@ -53,14 +64,41 @@ class InitiateMissionController extends Controller
         
     }
 
+    /**
+     * Get mission with his ID
+     *
+     * @param  integer $missionId
+     * @return Mission
+     */
     protected function getMission(int $missionId) : Mission {
         return Mission::where('id', $missionId)->first();
     }
 
+    /**
+     * Insert people in database
+     *
+     * @param  Collection $people
+     * @return void
+     */
     protected function insertPersonMission(Collection $people) : void {
         $people->each(
-            fn($person)=>MissionPerson::create([
+            fn($person)=> MissionPerson::create([
                 'person_id' => $person,
+                'mission_id' => $this->missionId
+            ])
+        );
+    }
+
+    /**
+     * Insert informations into database
+     *
+     * @param  Collection $hideouts
+     * @return void
+     */ 
+    protected function insertHideoutMission(Collection $hideouts) : void {
+        $hideouts->each(
+            fn($hideout) => MissionHideout::create([
+                'hideout_id' => $hideout,
                 'mission_id' => $this->missionId
             ])
         );
