31becddcd68fb35ce725ca2a95b276ec1877e57a
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-03-09 21:05:10 +0100

    ajout de secure_asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenue sur le site du KGB</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" />
 </head>
 <body class="h-full">
     <x-navigation.panel />
@@ -13,6 +13,6 @@
         @yield('content')
     </main>
     <footer></footer>
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
